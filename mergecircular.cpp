0fcac5c8950600bd29b6162fc687c487b643b11e
Mariam Kilany <125607324+Mariam-44@users.noreply.github.com>
2024-04-18 23:25:42 +0200

    Add files via upload

--- mergecircular.cpp
+++ mergecircular.cpp
@@ -1,74 +1,21 @@
- #include<iostream>
-using namespace std;
-struct Node {
-	int data;
-	Node* next;
-};
-class CircularLinkedList {
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
 public:
-	Node* rear;
-public:
-	CircularLinkedList() {
-		rear = nullptr;;
-	}
-	Node* MergeTwoLists(Node* list1, Node* list2) { //time= O(n+m) n=nodes in list1 m=nodes in list2 Space=O(1)
-		if (!list1) return list2;
-		if (!list2) return list1;
-		Node* list3 = nullptr;
-		Node* ptr = nullptr;
-
-		Node* l1 = list1;
-		Node* l2 = list2;
-		if (l1->data < l2->data) {
-			list3 = ptr = l1;
-			l1 = l1->next;
-		}
-		else if (l1->data == l2->data) {
-			list3 = ptr = l1;
-			l1 = l1->next;
-			l2 = l2->next;
-		}
-		else {
-			list3 = ptr = l2;
-			l2 = l2->next;
-		}
-
-		do {
-			if (l1->data < l2->data) {
-				ptr->next = l1;
-				l1 = l1->next;
-			}
-			else if (l1->data > l2->data) {
-				ptr->next = l2;
-				l2 = l2->next;
-			}
-			else {
-				ptr->next = l1;
-				l1 = l1->next;
-				l2 = l2->next;
-			}
-			ptr = ptr->next;
-		} while (l1 != list1->next && l2 != list2->next);
-
-		while (l1 != list1) {
-			ptr->next = l1;
-			l1 = l1->next;
-			ptr = ptr->next;
-		}
-		while (l2 != list2) {
-			ptr->next = l2;
-			l2 = l2->next;
-			ptr = ptr->next;
-		}
-
-		ptr->next = list3;
-
-		return list3;
-	}
-
-
-
+    bool hasCycle(ListNode* head) {
+        ListNode* runner = head;
+        while (runner && runner->next) {
+            runner = runner->next->next;
+            head = head->next;
+            if (runner == head)
+                return 1;
+        }
+        return 0;
+    }
 };
-int main() {
-	
-}
